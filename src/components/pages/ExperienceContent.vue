d8ef0e60d3dd2c87654d0723b78dba2b9605779e
riki-nakayashiki <riki.nakayashiki@gmail.com>
2023-07-06 10:40:16 -0700

    done with skills section

--- src/components/pages/ExperienceContent.vue
+++ src/components/pages/ExperienceContent.vue
@@ -1,6 +1,79 @@
 <template>
     <section class="contents" id="experience">
-        <h1>Experience</h1>
+        <h1>Projects</h1>
+        <section class="gallery">
+            <figure>
+                <img src="../../assets/rpa.png" alt="rpa">
+                <figcaption>
+                    <h3>RPA development Project</h3>
+                    <p>
+                        -Developed several virtual robots to automate customer's operation by myself including all steps
+                        from requirement definition, coding, testing to release task.
+                    </p>
+                    <p>
+                        -As a team leader, managed several teams to develop several virtual robots
+                    </p>
+                    <p>
+                        Tool:UiPath
+                    </p>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="../../assets/bpo.png" alt="bpo">
+                <figcaption>
+                    <h3>Optimization and automation of BPO operation</h3>
+                    <p>
+                        -Optimized business process outsourcing operation using several technologies
+                    </p>
+                    <p>
+                        -Developed a new website for BPO operation using Power Platform
+                    </p>
+                    <p>
+                        -Automated some part of operation
+                    </p>
+                    <p>
+                        Tool: Power platform(Power apps, power automate), UiPath, database
+                    </p>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="../../assets/internal.png" alt="internal">
+                <figcaption>
+                    <h3>Internal website development</h3>
+                    <p>
+                        -Developed a website for over 10,000 internal employees to track if they complete every single
+                        internal trainings
+                    </p>
+                    <p>
+                        Tool: Java, HTML, css, bootstrap and MySQL
+                    </p>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="../../assets/market-place.png" alt="market">
+                <figcaption>
+                    <h3>Website development for market place demo</h3>
+                    <p>
+                        -Developed a website for market place
+                    </p>
+                    <p>
+                        Tool: HTML, CSS, PHP, Vue.js, MySQL, Git
+                    </p>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="../../assets/Libraweb.png" alt="libraweb">
+                <figcaption>
+                    <h3>Website development for a library demo</h3>
+                    <p>
+                        -Developed a website for a library
+                    </p>
+                    <p>
+                        Tool: HTML, CSS, PHP, MySQL, Git
+                    </p>
+                </figcaption>
+            </figure>
+        </section>
     </section>
 </template>
 
--- src/components/pages/NavComponent.vue
+++ src/components/pages/NavComponent.vue
@@ -8,7 +8,7 @@
         <ul>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
-            <li><a href="#experience">Experience</a></li>
+            <li><a href="#experience">Projects</a></li>
             <li><a href="#skills">Skills</a></li>
             <li><a href="#contact">Contact</a></li>
         </ul>
