b061b26339500ad827628b765a48dc2f7f978351
riki-nakayashiki <riki.nakayashiki@gmail.com>
2023-07-09 14:05:08 -0700

    changed

--- src/components/pages/NavComponent.vue
+++ src/components/pages/NavComponent.vue
@@ -28,7 +28,8 @@
         </ul>
     </nav>
     <details class="smaller-menu">
-        <summary class="fa-solid fa-bars"></summary>
+        <!-- <summary class="fa-solid fa-bars"></summary> -->
+        <summary></summary>
         <ul>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
